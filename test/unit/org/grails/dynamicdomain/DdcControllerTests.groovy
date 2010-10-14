f3a69a787aace01663db2e6c7343945c27a0f391
limcheekin@vobject.com <limcheekin@vobject.com@2854b9da-b67c-354d-76e5-3cc0d1c2a7b5>
2010-10-14 16:06:34 +0000

    First import of dynamic generated views and controller based on dynamic domain class.

---
+++ test/unit/org/grails/dynamicdomain/DdcControllerTests.groovy
@@ -0,0 +1,17 @@
+package org.grails.dynamicdomain
+
+import grails.test.*
+
+class DdcControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
