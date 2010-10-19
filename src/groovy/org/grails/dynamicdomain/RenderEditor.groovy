c2e63f51d46511833c43e1c86b385f3b9f327c90
limcheekin@vobject.com <limcheekin@vobject.com@2854b9da-b67c-354d-76e5-3cc0d1c2a7b5>
2010-10-19 11:02:33 +0000

    Ready for 0.1 Released.

--- src/groovy/org/grails/dynamicdomain/RenderEditor.groovy
+++ src/groovy/org/grails/dynamicdomain/RenderEditor.groovy
@@ -1,9 +1,31 @@
+/* Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
 package org.grails.dynamicdomain
 
 import org.codehaus.groovy.grails.commons.ApplicationHolder as AH
 import org.codehaus.groovy.grails.web.pages.GroovyPagesTemplateEngine
 import org.codehaus.groovy.grails.web.pages.FastStringWriter
 
+  /**
+ * Work as runtime component of renderEditor.template.
+ * 
+ * @author <a href='mailto:limcheekin@vobject.com'>Lim Chee Kin</a>
+ *
+ * @since 0.1
+ */
 class RenderEditor {
 	void render(out, domainClass, property) {
 		if (property.type == Boolean.class || property.type == boolean.class)
@@ -185,7 +207,6 @@ class RenderEditor {
 	
 	// codes taken from org.codehaus.groovy.grails.web.taglib.AbstractGrailsTagTests
 	def applyTemplate(template, params = [:], target = null, String filename = null ) {
-		
 		GroovyPagesTemplateEngine engine = AH.application.mainContext.groovyPagesTemplateEngine
 		
 		def t = engine.createTemplate(template, filename ?: "renderEditor_"+ System.currentTimeMillis())
