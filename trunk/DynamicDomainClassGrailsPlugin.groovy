9697f41d1cdc706e308758ed86a6d0241098db3c
limcheekin@vobject.com <limcheekin@vobject.com@2854b9da-b67c-354d-76e5-3cc0d1c2a7b5>
2010-12-30 04:15:41 +0000

    Release the plugin 0.2.1 to support grails 1.2+ as per Burt's advice at https://code.google.com/p/grails-dynamic-domain-class-plugin/issues/detail?id=1

--- trunk/DynamicDomainClassGrailsPlugin.groovy
+++ trunk/DynamicDomainClassGrailsPlugin.groovy
@@ -21,11 +21,11 @@
  */
 class DynamicDomainClassGrailsPlugin {
     // the plugin version
-    def version = "0.2"
+    def version = "0.2.1"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.3.5 > *"
+    def grailsVersion = "1.2 > *"
     // the other plugins this plugin depends on
-    def dependsOn = [hibernate: "1.3.5 > *"]
+    def dependsOn = [hibernate: "1.2 > *"]
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/views/error.gsp"
